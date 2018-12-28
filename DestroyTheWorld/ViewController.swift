00a5a24275f9a26ceddb7d6a1957007567c3d8b9
Vuk Knežević <Vuk@Admin-Mac-mini.local>
2018-12-28 17:03:51 +0100

    Initial Commit

---
+++ DestroyTheWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DestroyTheWorld
+//
+//  Created by Vuk Knežević on 12/28/18.
+//  Copyright © 2018 Teodora Knežević. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
